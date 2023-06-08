7b62cca535db402ddaf1c9daddf7fe7fa3adb9f4
V Suhas <suhas3145@gmail.com>
2023-06-08 22:31:36 +0530

    register user front-end setup done

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -2,21 +2,34 @@ import React,{useReducer,useContext} from "react";
 
 
 import reducer from "./reducer";
+import axios from 'axios'
 import { 
     DISPLAY_ALERT ,
      CLEAR_ALERT,
      REGISTER_USER_BEGIN,
      REGISTER_USER_SUCCESS,
-     REGISTER_USER_ERROR} from "./actions";
-     
+     REGISTER_USER_ERROR
+    } from "./actions";
+
+
+
+
+const token = localStorage.getItem('token')
+const user = localStorage.getItem('user')
+const userLocation = localStorage.getItem('location')
+
+
+
+
 const initialState={
     isLoading:false,
     showAlert:false,
     alertText:'',
     alertType:'',
-    user:null,
+    user:user ? JSON.parse(user):null,
     token:null,
-    userLocation:'',
+    userLocation:userLocation||'',
+    jobLocation:userLocation||'',
 }
 
 const AppContext = React.createContext()
@@ -38,8 +51,43 @@ const AppProvider = ({children})=>{
 
     }
 
+    const addUserToLocalStorage = ({user,token,location})=>{
+        localStorage.setItem('user',JSON.stringify(user))
+        localStorage.setItem('token',token)
+        localStorage.setItem('location',location)
+    }
+
+    const removeUserFromLocalStorage=()=>{
+        localStorage.removeItem('token')
+        localStorage.removeItem('user')
+        localStorage.removeItem('location')
+    }
+
+
+
+
+
     const registerUser = async (currentUser)=>{
-        console.log(currentUser)
+           dispatch({type: REGISTER_USER_BEGIN})
+           try{
+            const response = await axios.post('/api/v1/auth/register',currentUser)
+            // console.log(response)
+            const {user,token,location} = response.data
+            dispatch({type:REGISTER_USER_SUCCESS,
+                payload:{user,token,location},
+            })
+            addUserToLocalStorage({user,token,location})
+           }catch(error){
+            // console.log(error.response)
+            dispatch({
+                type: REGISTER_USER_ERROR,
+                payload: {msg : error.response.data.msg},
+            })
+
+
+           }
+
+           clearAlert()
     }
 
 
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import { Logo, FormRow, Alert } from '../components'
 import Wrapper from '../assets/wrappers/RegisterPage'
 import { useAppContext } from '../context/appContext'
-
+import {useNavigate} from 'react-router-dom'
 
 const initialState = {
   name: '',
@@ -12,9 +12,10 @@ const initialState = {
 }
 
 const Register = () => {
-
+  const navigate = useNavigate()
   const [values, setValues] = useState(initialState)
-  const {isLoading,showAlert,displayAlert,registerUser}= useAppContext()
+  
+  const {user,isLoading,showAlert,displayAlert,registerUser}= useAppContext()
   
 
 
@@ -43,6 +44,15 @@ const Register = () => {
 
   
   }
+
+  useEffect(()=>{
+   if(user){
+    setTimeout(()=>{
+      navigate('/')
+    },3000)
+    
+   }
+  },[user,navigate])
   return (
     <Wrapper className='full-page'>
       <form className='form' onSubmit={onSubmit}>

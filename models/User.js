7865ebda315bef4b9dc140de838caeff8c2b0ecd
V Suhas <suhas3145@gmail.com>
2023-06-09 21:53:32 +0530

    login server setup done with errors

--- models/User.js
+++ models/User.js
@@ -52,7 +52,17 @@ UserSchema.pre('save',async function(){
 })
 
 UserSchema.methods.createJWT = function(){
-    return jwt.sign({userId:this._id},process.env.JWT_SECRET,{expiresIn:process.env.JWT_LIFETIME})
+    return jwt.sign({userId:this._id},process.env.JWT_SECRET,{expiresIn:process.env.JWT_LIFETIME,})
 }
+
+UserSchema.methods.comparePassword = async function(candidatePassword){
+    const isMatch = await bcrypt.compare(candidatePassword,this.password)
+    
+    return isMatch
+    
+}
+
+
+
 
 export default mongoose.model('User', UserSchema)
